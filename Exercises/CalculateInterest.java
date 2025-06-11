0905711bad560b2001de98e53b689ea625d67c9c
ayomidesangogade <160060831+ayomidesangogade@users.noreply.github.com>
2025-06-11 23:45:21 +0100

    Today's exercise

---
+++ Exercises/CalculateInterest.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class CalculateInterest {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter balance and interest rate (e.g., 3 for 3%): ");
+        double balance = input.nextDouble();
+        double annualInterestRate = input.nextDouble();
+        double interest = balance*(annualInterestRate/1200);
+        input.close();
+        System.out.println("The interest is "+(float)interest);
+    }
+}
