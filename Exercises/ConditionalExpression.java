d1742e2a44e3633fc1cab26d5b92c6512431628d
ayomidesangogade <160060831+ayomidesangogade@users.noreply.github.com>
2025-06-20 23:02:17 +0100

    Today's Exercise

---
+++ Exercises/ConditionalExpression.java
@@ -0,0 +1,8 @@
+public class ConditionalExpression {
+    public static void main(String[]args){
+        // java.util.Scanner input = new java.util.Scanner(System.in);
+        // System.out.print("Enter income: ");
+        System.out.println(0b1011);
+        // input.close();
+    }
+}
