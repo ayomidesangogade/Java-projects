fe198dfa7c321e31beb6000ba9a8502b3f82ed67
ayomidesangogade <160060831+ayomidesangogade@users.noreply.github.com>
2025-06-23 22:12:10 +0100

    Today's exercise

---
+++ Exercises/CostOfDriving.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class CostOfDriving{
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the driving distance: ");
+        double drivingDistance = input.nextDouble();
+        System.out.print("Enter miles per gallon: ");
+        double milesPerGallon = input.nextDouble();
+        System.out.print("Enter price per gallon: ");
+        double pricePerGallon = input.nextDouble();
+        double gallon = drivingDistance/milesPerGallon;
+        double price = gallon*pricePerGallon;
+        System.out.println("The cost of driving is $"+(float)price);
+        input.close();
+    }
+}
