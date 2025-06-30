e099fc714b96be55c536a4ce2ba4bf74dd46a384
ayomidesangogade <160060831+ayomidesangogade@users.noreply.github.com>
2025-06-30 21:58:09 +0100

    Today's exercise

---
+++ Exercises/Employee1.java
@@ -0,0 +1,8 @@
+public class Employee1{
+    public static double salary;
+    public static final String DEPARTMENT = "Development ";
+    public static void main(String[]args){
+        salary = 1000;
+        System.out.println(DEPARTMENT+"average salary:"+salary);
+    }
+}
---
+++ Exercises/EvenOdd.java
@@ -0,0 +1,18 @@
+// SANGOGADE AYOMIDE EPHRAIM
+// 223322
+// Level 200
+
+import java.util.Scanner;
+public class EvenOdd {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number: "); 
+        int number = input.nextInt(); // this takes the number into the system
+        if (number%2 == 0) // this checks whether the number is divisible by two or not
+            System.out.println(number+" is an even number."); // this executes, if true
+        else
+            System.out.println(number+" is an odd number.");// this executes, if false
+        input.close();
+    }
+}
