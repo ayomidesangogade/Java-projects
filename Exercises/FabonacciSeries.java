a7acc52b9302101de919b4d78a97befaff330e0a
ayomidesangogade <160060831+ayomidesangogade@users.noreply.github.com>
2025-07-02 23:01:12 +0100

    Today's exercise

---
+++ Exercises/FabonacciSeries.java
@@ -0,0 +1,24 @@
+// SANGOGADE AYOMIDE
+// 223322
+// level 200
+import java.util.Scanner;
+
+public class FabonacciSeries{
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of series you want: ");
+        int num = input.nextInt();
+        int firstNumber = 0;
+        int secondNumber = 1;
+        System.out.print(""+firstNumber+" "+secondNumber+"");
+        input.close();
+        int i;
+        for(i = 0; i<=num-3;i++){
+            int next = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = next;
+            System.out.print(" "+(next));
+        }
+    } 
+}
