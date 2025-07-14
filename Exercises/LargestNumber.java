5c1f6f632e448832664cc6e0fe1f2b1300f10103
ayomidesangogade <160060831+ayomidesangogade@users.noreply.github.com>
2025-07-14 23:20:33 +0100

    Today's Exercise

---
+++ Exercises/LargestNumber.java
@@ -0,0 +1,31 @@
+// SANGOGADE AYOMIDE
+// 223322
+// Level 200
+
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        // this takes in the number of values
+        System.out.print("Enter number of values: ");
+        int num = input.nextInt();
+
+        double [] numbers = new double[num];
+        //this stores the numbers in the array.
+        for (int i = 0; i < num; i++){
+            System.out.print("Enter number "+(i+1)+": ");
+            numbers[i] = input.nextDouble();
+        }
+
+        double largestNum = numbers[0];
+        // if any number in the array is greater than the variable(largestNum), it becomes the largest number.
+        for (int j = 0; j < numbers.length; j++){
+            if (numbers[j] > largestNum){
+                largestNum = numbers[j];
+            }  
+        } 
+        System.out.println("The largest number is: "+largestNum);
+        input.close();
+    }
+}
