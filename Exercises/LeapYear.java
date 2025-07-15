7a090df0f05e4e02b99d9fb8f3755f51ae27ff6d
ayomidesangogade <160060831+ayomidesangogade@users.noreply.github.com>
2025-07-15 22:29:09 +0100

    Today's Exercise

--- Exercises/LeapYear.java
+++ Exercises/LeapYear.java
@@ -1,10 +1,10 @@
-// import java.util.Scanner;
-// public class LeapYear {
-//     Scanner input = new Scanner(System.in);
+import java.util.Scanner;
+public class LeapYear {
+    Scanner input = new Scanner(System.in);
 
-//     System.out.print("Enter year: ");
-//     int year = input.nextInt();
+    System.out.print("Enter year: ");
+    int year = input.nextInt();
 
-//     boolean isLeapYear = (year%4 == 0 && year % 100 != 0)||(year%400 == 0);
-//     System.out.println(year+" is a leap year? "+ isLeapYear);
-// }
+    boolean isLeapYear = (year%4 == 0 && year % 100 != 0)||(year%400 == 0);
+    System.out.println(year+" is a leap year? "+ isLeapYear);
+}
