f4ca4edc67b5843fef5c1e1fff88f6bcaaa73d62
ayomidesangogade <160060831+ayomidesangogade@users.noreply.github.com>
2025-08-13 23:54:18 +0100

    Today's Exercise

---
+++ Exercises/PalindromeBeginner.java
@@ -0,0 +1,14 @@
+public class PalindromeBeginner {
+    public static void main(String[]args){
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        System.out.print("Enter a three-digit integer: ");
+        int integer = input.nextInt();
+        int lastDigit = integer%10;
+        int firstDigit = integer/100;
+        if(lastDigit == firstDigit)
+            System.out.println(integer+" is a palindrome.");
+        else
+            System.out.println(integer+" is not a palindrome.");
+        input.close();
+    }
+}
